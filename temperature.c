1038e3ab7fc73472e68e5f83dbc38a2d3462f3fd
apeg3200 <apeg3200@batman.dev.tricerat.com>
2017-12-20 11:47:49 -0500

    refactored temperature.c

--- temperature.c
+++ temperature.c
@@ -1,19 +1,13 @@
 #include <stdio.h>
 
-// print Fahrenheit-Celsius table
+// define constants -> #define
+#define LOWER 0
+#define UPPER 300
+#define STEP  20
+
 int main () {
-  int fahr, celsius;
-  int lower, upper, step;
-
-  lower = 0;
-  upper = 300;
-  step = 20;
-
-  fahr = lower;
-
-  while (fahr <= upper) {
-    celsius = 5 * (fahr - 32) / 9;
-    printf("%d,\t%d\n", fahr, celsius);
-    fahr = fahr + step;
+  int fahr;
+  for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP) {
+    printf("%3d %6.1f\n", fahr, (5.0/9.0) * (fahr - 32));
   }
 }
